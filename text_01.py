2d04d20081215478987dd0084c33b4a32d84a8f7
MenukaSankalpa <menuka.sankalpa@gmail.com>
2024-12-28 18:49:40 +0530

    add more qustions

--- text_01.py
+++ text_01.py
@@ -7,9 +7,38 @@ if playing != "yes":
 
 print("Okey! Let's play :) ")
 
-answer = input("What is a computer? ")  
-if answer == "A machine that processes and stores information":
+answer = input("What is part Memory that stores temporary data? ")  
+if answer == "ram":
     print('Correct!') 
     
 else:
     print("Incorrect!")
+
+answer = input("The physical parts of a computer.what is that? ")  
+if answer == "hardware":
+    print('Correct!') 
+    
+else:
+    print("Incorrect!")
+    
+answer = input("What is he brain of the computer? ")  
+if answer == "cpu":
+    print('Correct!') 
+    
+else:
+    print("Incorrect!")   
+    
+answer = input("A copy of data for safety.What is it call? ")  
+if answer == "backup":
+    print('Correct!') 
+    
+else:
+    print("Incorrect!") 
+
+answer = input("What call the name of Harmful software that can damage a computer. ")  
+if answer == "virus":
+    print('Correct!') 
+    
+else:
+    print("Incorrect!")     
+         
