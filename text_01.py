5778e94065ea06aaee2a0627fdcf06c576c6cd9c
MenukaSankalpa <menuka.sankalpa@gmail.com>
2024-12-28 18:57:00 +0530

    Finish game

--- text_01.py
+++ text_01.py
@@ -2,43 +2,50 @@ print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play: ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()   
 
 print("Okey! Let's play :) ")
+score = 0
 
 answer = input("What is part Memory that stores temporary data? ")  
-if answer == "ram":
+if answer.lower() == "ram":
     print('Correct!') 
+    score += 1
     
 else:
     print("Incorrect!")
 
 answer = input("The physical parts of a computer.what is that? ")  
-if answer == "hardware":
-    print('Correct!') 
+if answer.lower() == "hardware":
+    print('Correct!')
+    score += 1 
     
 else:
     print("Incorrect!")
     
 answer = input("What is he brain of the computer? ")  
-if answer == "cpu":
+if answer.lower() == "cpu":
     print('Correct!') 
+    score += 1
     
 else:
     print("Incorrect!")   
     
 answer = input("A copy of data for safety.What is it call? ")  
-if answer == "backup":
-    print('Correct!') 
+if answer.lower() == "backup":
+    print('Correct!')
+    score += 1 
     
 else:
     print("Incorrect!") 
 
 answer = input("What call the name of Harmful software that can damage a computer. ")  
-if answer == "virus":
-    print('Correct!') 
+if answer.lower() == "virus":
+    print('Correct!')
+    score += 1 
     
 else:
     print("Incorrect!")     
-         
+
+print("You Got " + str(score) + "questions correct! ")         
